6890139f29a305ee711d2d2074983247960729e6
Ankit <ankit181998anand@gmail.com>
2023-06-01 20:44:09 +0530

    setup react tutorials

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Greet = () =>
+{
+  
+  
+  return <h1>Hallo!!!</h1>
+}
+
+
+ReactDOM.render(<Greet />,document.getElementById('root'))
+
