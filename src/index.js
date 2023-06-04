d743773a1ea87bb5b0fa4bb10a1dd8267bddf345
Ankit <ankit181998anand@gmail.com>
2023-06-04 15:47:48 +0530

    1-Class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Greet = () =>
-{
-  
-  
-  return <h1>Hallo!!!</h1>
-}
+//css
+import "./index.css";
 
+const Booklist = () => {
+  return (
+    <section className="BookList">
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-ReactDOM.render(<Greet />,document.getElementById('root'))
+const Book = () => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL600_SR600,400_.jpg"
+        alt="img"
+      />
+      <h1>Rich Dad Poor Dad</h1>
+      <h4>Robert T. Kiyosaki</h4>
+    </article>
+  );
+};
 
+ReactDOM.render(<Booklist />, document.getElementById("root"));
